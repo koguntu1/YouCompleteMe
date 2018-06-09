6ada9e6cc1844ea2d5a4379288d3e6bd9e6b1765
Hayden Smith <hsmith4@my.westga.edu>
2018-06-09 11:56:08 -0400

    Cosmetic changes to login form

--- YouCompleteMe/Views/loginForm.Designer.cs
+++ YouCompleteMe/Views/loginForm.Designer.cs
@@ -54,9 +54,9 @@
             this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(83, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 19);
+            this.label2.Size = new System.Drawing.Size(87, 19);
             this.label2.TabIndex = 1;
-            this.label2.Text = "User";
+            this.label2.Text = "User Name:";
             // 
             // label3
             // 
@@ -64,9 +64,9 @@
             this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(83, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 19);
+            this.label3.Size = new System.Drawing.Size(78, 19);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Password";
+            this.label3.Text = "Password:";
             // 
             // textBox1
             // 
@@ -83,6 +83,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(200, 27);
             this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // btnLogin
             // 
