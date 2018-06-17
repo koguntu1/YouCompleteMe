191ad9e08f76f424535ad234c343af3f5c9dc96f
Mandi Matchett <mmatche1@my.westga.edu>
2018-06-16 22:04:24 -0400

    Added formatted date functions and

--- YouCompleteMe/Views/HomepageForm.cs
+++ YouCompleteMe/Views/HomepageForm.cs
@@ -25,5 +25,19 @@ namespace YouCompleteMe.Views
             dateForm dateForm = new dateForm(user);
             dateForm.Show();
         }
+
+        // Returns the date selected by the user in a formatted string:
+        // Saturday, June 16, 2018
+        public String getSelectedDate_Formatted()
+        {
+            return monthCalendar1.SelectionRange.Start.ToString("D");
+        }
+
+        // Returns a the date selected by the user in a short string format:
+        // 06/16/2018
+        public String getSelectedDate()
+        {
+            return monthCalendar1.SelectionRange.Start.ToShortDateString();
+        }
     }
 }
