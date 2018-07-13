6b4c9afa8dd268a273818bdf1b32b5d20bc94a9b
Hayden Smith <hsmith4@my.westga.edu>
2018-07-13 12:46:11 -0400

    Adding files to track

---
+++ YouCompleteMe/ViewTimeSpentOnTaskForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YouCompleteMe.Models;
+using YouCompleteMe.Views;
+
+namespace YouCompleteMe
+{
+    public partial class ViewTimeSpentOnTaskForm : Form
+    {
+        private static CompletedTaskParameterForm parameter;
+        private User theUser;
+
+        public ViewTimeSpentOnTaskForm(CompletedTaskParameterForm aParameter, User aUser)
+        {
+            InitializeComponent();
+            parameter = aParameter;
+            theUser = aUser;
+        }
+
+        private void ViewTimeSpentOnTaskForm_Load(object sender, EventArgs e)
+        {
+            this.tasksTableAdapter.Fill(this.TimeSpentDataSet.tasks, theUser.userID, parameter.getStart(), parameter.getEnd());
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
