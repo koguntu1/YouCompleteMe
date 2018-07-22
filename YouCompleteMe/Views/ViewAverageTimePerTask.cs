d75acc45c196abd5da0beb73276774b284857289
Mandi Matchett <mmatche1@my.westga.edu>
2018-07-22 15:14:41 -0400

    Formatted total and average times to show hour/minute format

--- YouCompleteMe/Views/ViewAverageTimePerTask.cs
+++ YouCompleteMe/Views/ViewAverageTimePerTask.cs
@@ -31,6 +31,7 @@ namespace YouCompleteMe.Views
             dataGridView1.AutoGenerateColumns = true;
             dataGridView1.AutoResizeColumns();// = true;
             dataGridView1.DataSource = ds.Tables["tasks"];
+            dataGridView1.ReadOnly = true;
 
             dataGridView1.Columns[4].Visible = false;
             dataGridView1.Columns[5].Visible = false;
@@ -40,6 +41,32 @@ namespace YouCompleteMe.Views
             dataGridView1.Columns[1].HeaderText = "Number of Sessions";
             dataGridView1.Columns[2].HeaderText = "Average Time";
             dataGridView1.Columns[3].HeaderText = "Total Time";
+
+            dataGridView1.CellFormatting += DataGridView1_CellFormatting;
+        }
+
+        private void DataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Column2")
+            {
+                double minutes = Convert.ToDouble(e.Value) / 60.0;
+                if (minutes < 1)
+                    e.Value = "< 1 minute";
+                else if (minutes > 60)
+                    e.Value = (int)(minutes / 60) + " hour(s) and " + (int)minutes % 60 + " minutes";
+                else
+                    e.Value = (int)minutes + " minutes";
+            }
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "Column3")
+            {
+                double minutes = Convert.ToDouble(e.Value) / 60.0;
+                if (minutes < 1)
+                    e.Value = "< 1 minute";
+                else if (minutes > 60)
+                    e.Value = (int)(minutes / 60) + " hour(s) and " + (int)minutes % 60 + " minutes";
+                else
+                    e.Value = (int)minutes + " minutes";
+            }
         }
     }
 }
